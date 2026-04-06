65de585dd323ea0839364ef4842da4cb9d45b576
Holden Gent <95231434+HoldenGent@users.noreply.github.com>
2026-04-06 17:43:29 +0000

    TODOs

--- CommitCafe.java
+++ CommitCafe.java
@@ -1,17 +1,20 @@
 public class CommitCafe {
 
-    private static String barista = "Nadine"; // STUDENT_TODO_1A: Change name
-    // STUDENT_TODO_1B: Add a nickname or title for the barista
+    private static String barista = "Holden";
+    private static String nickname = "Hold"; // nickname for the barista
 
     private static int cups = 0;
 
     public static void brew(String drink) {
         // STUDENT_TODO_2A: Implement - add 1 to cups & print a message
+        cups++;
+        System.out.println(barista + " brewed a cup of " + drink + ". Total cups brewed: " + cups);
+        
         // STUDENT_TODO_2B: Add a second brew-related improvement
     }
 
     public static void printSummary() {
-        System.out.println("[SUMMARY] " + barista + " brewed " + cups + " cups today.");
+        System.out.println("[SUMMARY] " + barista + " (aka " + nickname + ") brewed " + cups + " cups today.");
     }
 
     public static void main(String[] args) {
